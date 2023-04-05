38e47a92e951071f5dab097e37ae17e33dc508d2
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-04-05 17:33:29 -0400

    Create Start.js

---
+++ components/Start.js
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+import { StyleSheet, TouchableOpacity, View, Text, Button, TextInput, ImageBackground } from 'react-native';
+
+const Start = ({ navigation }) => {
+  const [name, setName] = useState('');
+  const [bgColor, setBgColor] = useState('');
+
+  const handleColorPress = (color) => {
+    setBgColor(color);
+    navigation.setParams({ bgColor: color });
+  };
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../assets/img2.jpg')}
+        style={styles.backgroundImage}
+      >
+        <View style={styles.overlay}>
+          <Text style={styles.title}>Welcome to ChatApp</Text>
+          <TextInput
+            style={styles.textInput}
+            value={name}
+            onChangeText={setName}
+            placeholder='Enter your name'
+            placeholderTextColor='#8e8e8e'
+          />
+          <View>
+            <Text style={styles.colorSelector} >Choose your Background:</Text>
+            <View style={styles.colorWrapper}>
+              <TouchableOpacity
+                style={[styles.colorButton, { backgroundColor: '#FFFFFF' }]}
+                onPress={() => handleColorPress('#FFFFFF')}
+              />
+              <TouchableOpacity
+                style={[styles.colorButton, { backgroundColor: '#C1C1C1' }]}
+                onPress={() => handleColorPress('#C1C1C1')}
+              />
+              <TouchableOpacity
+                style={[styles.colorButton, { backgroundColor: '#FFC0CB' }]}
+                onPress={() => handleColorPress('#FFC0CB')}
+              />
+              <TouchableOpacity
+                style={[styles.colorButton, { backgroundColor: '#87CEFA' }]}
+                onPress={() => handleColorPress('#87CEFA')}
+              />
+            </View>
+          </View>
+          <Button
+            title='Start Chatting'
+            onPress={() => navigation.navigate('Chat', { name: name, bgColor: bgColor })}/>
+
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 20,
+    borderRadius: 10,
+    margin: 20,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: 30,
+  },
+  textInput: {
+    backgroundColor: '#fff',
+    width: '100%',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+  },
+  colorSelector: {
+    color: '#fff',
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  colorWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  colorButton: {
+    width: 30,
+    height: 30,
+    borderRadius:15,
+  },
+});
+
+export default Start;
