e43094b66b2f7986eba076fa99b668f557fc2d1b
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-04-18 14:54:53 -0400

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,12 @@
 import { useState, useEffect } from "react";
-import { StyleSheet, View, Text, KeyboardAvoidingView, Platform} from 'react-native';
+import { StyleSheet, View, KeyboardAvoidingView, Platform} from 'react-native';
 import { Bubble, GiftedChat } from "react-native-gifted-chat";
-const Chat = ({ route, navigation }) => {
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
+
+
+
+const Chat = ({ route, navigation, db }) => {
+
   const { name, bgColor } = route.params;
   const [messages, setMessages] = useState([]);
 // function to handle sending new messages
@@ -10,24 +15,22 @@ const Chat = ({ route, navigation }) => {
   }
 // set up initial message and the new system message
 useEffect(() => {
-  setMessages([
-    {
-      _id: 1,
-      text: 'Hello developer',
-      createdAt: new Date(),
-      user: {
-        _id: 2,
-        name: 'React Native',
-        avatar: 'https://placeimg.com/140/140/any',
-      },
-    },
-    {
-      _id: 2,
-      text: 'This is a system message',
-      createdAt: new Date(),
-      system: true,
-    },
-  ]);
+  navigation.setOptions({ title: name });
+  const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+  const unsubMessages = onSnapshot(q, (docs) => {
+    let newMessages = [];
+    docs.forEach(doc => {
+      newMessages.push({
+        id: doc.id,
+        ...doc.data(),
+        createdAt: new Date(doc.data().createdAt.toMillis())
+      })
+    })
+    setMessages(newMessages);
+  })
+  return () => {
+    if (unsubMessages) unsubMessages();
+  }
 }, []);
 
 //changing the speech bubble color 
