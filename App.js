4d74e31408d394b65251766cc0f486ef36b7d477
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-04-18 14:54:56 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
 // import the screens
 import Start from './components/Start';
 import Chat from './components/Chat';
@@ -8,7 +11,26 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAMqDPHKGafvM0X1HVW0XnrvDSRbSonkRg",
+    authDomain: "chatapp-61cc6.firebaseapp.com",
+    projectId: "chatapp-61cc6",
+    storageBucket: "chatapp-61cc6.appspot.com",
+    messagingSenderId: "221802851166",
+    appId: "1:221802851166:web:33bbc5bdd47e85f8ec7dcf",
+    measurementId: "G-5JN8HZCZMX"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -20,7 +42,7 @@ const App = () => {
         />
         <Stack.Screen
           name="Chat"
-          component={Chat}
+          component={props => <Chat db={db} {...props} />}
         />
       </Stack.Navigator>
     </NavigationContainer>
