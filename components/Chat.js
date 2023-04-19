5812c32cca1dd99c72486d3504a2cdb4dafbee8e
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2023-04-19 14:50:01 -0400

    Update Chat.js
    - utilize the isConnected prop to decide what source to fetch data from.
    
    -fetch messages from the Firestore database and cache messages whenever possible in an onSnapshot callback function (involves AsyncStorage.setItem()).

--- components/Chat.js
+++ components/Chat.js
@@ -1,56 +1,93 @@
 import { useState, useEffect } from "react";
 import { StyleSheet, View, KeyboardAvoidingView, Platform} from 'react-native';
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { Bubble, GiftedChat , InputToolbar} from "react-native-gifted-chat";
 import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
 
   const { name, bgColor } = route.params;
   const [messages, setMessages] = useState([]);
-// function to handle sending new messages
-  const onSend = (newMessages) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
-  }
-// set up initial message and the new system message
-useEffect(() => {
-  navigation.setOptions({ title: name });
-  const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-  const unsubMessages = onSnapshot(q, (docs) => {
-    let newMessages = [];
-    docs.forEach(doc => {
-      newMessages.push({
-        id: doc.id,
-        ...doc.data(),
-        createdAt: new Date(doc.data().createdAt.toMillis())
-      })
-    })
-    setMessages(newMessages);
-  })
-  return () => {
-    if (unsubMessages) unsubMessages();
-  }
-}, []);
+  let unsubMessages
 
-//changing the speech bubble color 
-const renderBubble = (props) => {
-  return <Bubble
-    {...props}
-    wrapperStyle={{
-      right: {
-        backgroundColor: "#000"
-      },
-      left: {
-        backgroundColor: "#FFF"
-      }
-    }}
-  />
-}
-// set up navigation options
+
+  // function to handle sending new messages
+  const onSend = (newMessages) => {
+      setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
+
+  // set up initial message and the new system message
   useEffect(() => {
-    navigation.setOptions({ title: name,  backgroundColor: bgColor  });
-  }, []);
+    if(isConnected) {
+    navigation.setOptions({ title: name });
+    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+    unsubMessages = onSnapshot(q, (docs) => {
+      let newMessages = [];
+      docs.forEach(doc => {
+        newMessages.push({
+          id: doc.id,
+          ...doc.data(),
+          createdAt: new Date(doc.data().createdAt.toMillis())
+        })
+      })
+      cacheMessages(newMessages);
+      setMessages(newMessages);
+    })
+  } else {
+    loadCachedMessages();
+  }
+    return () => {
+      if (unsubMessages) unsubMessages();
+    }
+  }, [isConnected]);
+
+
+  const cacheMessages = async (messagesToCache) => {
+    try {
+        await AsyncStorage.setItem('messages', JSON.stringify(messagesToCache));
+    } catch (error) {
+        console.log(error.message);
+    };
+  }
+
+  const loadCachedMessages = async () => {
+    try {
+      const cachedMessages = await AsyncStorage.getItem('messages');
+      if (cachedMessages) {
+        setMessages(JSON.parse(cachedMessages));
+      }
+    } catch (error) {
+      console.log('Error loading cached messages:', error.message);
+    }
+  }
+
+  
+  const renderInputToolbar = (props) => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return null;
+  }
+  
+  //changing the speech bubble color 
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000"
+        },
+        left: {
+          backgroundColor: "#FFF"
+        }
+      }}
+    />
+  }
+
+// // set up navigation options
+//   useEffect(() => {
+//     navigation.setOptions({ title: name,  backgroundColor: bgColor  });
+//   }, []);
 
   return (
 
@@ -59,6 +96,7 @@ const renderBubble = (props) => {
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
         onSend={messages => onSend(messages)}
         user={{
           _id: 1
